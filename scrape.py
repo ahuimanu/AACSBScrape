3d3ff4d6f716d8d7103e5fa5682598834fc971c5
Jeffry Babb <ahuimanu@gmail.com>
2021-08-27 11:40:53 -0500

    updated README

--- scrape.py
+++ scrape.py
@@ -7,6 +7,8 @@ def main():
     school_url = "https://www.mba.today/school/alabama-a-and-m-university"
     print(get_payload(school_url))
 
+def soup_load(page_text):
+    soup = BeautifulSoup(page_text, 'html.parser')
 
 def get_payload(url):
     return requests.get(url).text
