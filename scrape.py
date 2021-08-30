062ccecb3abcc401b3202bef70669b0fb1e5f1b2
Jeffry Babb <ahuimanu@gmail.com>
2021-08-30 18:14:16 -0500

    saving to file

--- scrape.py
+++ scrape.py
@@ -1,11 +1,13 @@
-import time
+import io
 import random
+import time
 
 import requests
 from bs4 import BeautifulSoup
+from bs4 import element
 
 # mba today prefix
-mba_today_prefix = "https://www.mba.today/"
+mba_today_prefix = "https://www.mba.today"
 aacsb_schools_url = "https://www.mba.today/guide/aacsb-accredited-business-schools"
 united_states_canada_id = "usa-canada"
 
@@ -32,18 +34,52 @@ class School:
         return f"name: {self.name}\n" f"link: {self.url}\n"
 
 
-def main():
-    # screen scraping to get AACSB schools
-    print("SCRAPING")
+def get_addresses():
 
-    # create
-    make_schools()
-    get_addresses()
+    # work with schools
+    for school in schools_list:
 
-    # show schools
-    # print(f"how many schools: {len(schools_list)}")
-    # for school in schools_list:
-    #     print(school)
+        if school.url == "https://www.mba.today/school/":
+            continue
+        else:
+            page_text = get_payload(school.url)
+            soup = get_soup_from_text(page_text)
+
+            # pause somewhat randomly
+            timeout = random.uniform(0.5, 1.5)
+            time.sleep(timeout)
+
+            schoolinfo = soup.find_all(
+                attrs={"itemtype": "http://schema.org/CollegeOrUniversity"}
+            )
+            output = ""
+            for child in schoolinfo[0].descendants:
+                if isinstance(child, element.NavigableString):
+                    output += str(child.string) + "\n"
+
+            # show output
+            school_text = (
+                "================================================================================"
+                + "\n"
+            )
+            school_text += parse_address(output)
+            school_text += (
+                "================================================================================"
+                + "\n"
+            )
+            # status
+            print(f"processed: {school.name}")
+
+            # write output string to file
+            write_schools_to_file("aacsb_addresses.txt", school_text)
+
+
+def get_soup_from_text(page_text):
+    return BeautifulSoup(page_text, "html.parser")
+
+
+def get_payload(url):
+    return requests.get(url).text
 
 
 def make_schools():
@@ -59,33 +95,54 @@ def make_schools():
         schools_list.append(School(link.string, f"{mba_today_prefix}{link['href']}"))
 
 
-def get_addresses():
+def parse_address(address_text):
+    parts = address_text.split("\n")
+    essential_parts = []
+    output = ""
+    for part in parts:
+        if len(part) > 1 and not part.startswith(","):
+            essential_parts.append(part)
 
-    # work with schools
+    for part in essential_parts:
+        output += f"{part}\n"
+
+    return output
+
+
+def print_schools():
+    # print school list to screen
+    print(f"how many schools: {len(schools_list)}")
+
     for school in schools_list:
         print(school)
-        page_text = get_payload(school.url)
-        soup = get_soup_from_text(page_text)
-
-        # pause somewhat randomly
-        timeout = random.uniform(0.5, 2.0)
-        time.sleep(timeout)
-
-        address = soup.find_all("address")
-        print(address)
-        # print(f"how many addresses {len(address)}")
 
 
-def parse_address(address_text):
-    pass
+def write_schools_to_file(filename, school_text):
+    # write schools to file
+    with io.open(filename, "a") as f:
+        f.write(school_text)
 
 
-def get_soup_from_text(page_text):
-    return BeautifulSoup(page_text, "html.parser")
+def write_schools_to_textfile():
+    # write schools list to file
+    output = ""
+    for school in schools_list:
+        output += str(school) + "\n"
+
+    # write schools to file
+    f = open("schools.txt", "w")
+    f.write(output)
+    f.close()
 
 
-def get_payload(url):
-    return requests.get(url).text
+def main():
+    # screen scraping to get AACSB schools
+    print("SCRAPING")
+
+    # create
+    make_schools()
+
+    get_addresses()
 
 
 if __name__ == "__main__":
