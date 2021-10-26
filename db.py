af148c1835edb8f7c5830ce642befd10331254df
Jeffry Babb <ahuimanu@gmail.com>
2021-10-26 10:20:43 -0500

    updated db code

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 
 from models import Base, College
 
-conn_string = 'sqlite:///foo.db'
+conn_string = 'sqlite:///schools.db'
 engine = create_engine(conn_string)
 
 def init_db():
